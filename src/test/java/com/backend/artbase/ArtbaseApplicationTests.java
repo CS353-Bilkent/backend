bd9fa02809336ae73074b72a2c60b359ea3b1bdd
Mert <gencturkmert1@gmail.com>
2023-10-23 22:44:00 +0300

    initial commit

---
+++ src/test/java/com/backend/artbase/ArtbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.artbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
