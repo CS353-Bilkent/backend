cbdb0d0471ca6482d7a40f57ac411a2a3f9da0f5
Mert <gencturkmert1@gmail.com>
2023-12-20 17:15:29 +0300

    admin controller todo

--- src/main/java/com/backend/artbase/controllers/AdminController.java
+++ src/main/java/com/backend/artbase/controllers/AdminController.java
@@ -10,4 +10,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AdminController {
 
+    /*
+     * TODO Approve Auction Approve Payment Look up old auctions, sysdate > end
+     * date look up bid statistics per artwork, how many bid, max value, length
+     * of auction etc. use group by, max, count Look up user statistics, artwork
+     * group by artist, Look up workshops //Workshopları atıcam yarın Look up
+     * collection statistics, artwork added to any collection group by artist
+     * Look up old sales etc
+     */
+
 }
