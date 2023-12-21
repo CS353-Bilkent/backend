9b591e535e2712941ba255ae2627d89c43a91229
Faruk Ulutaş <faruk.ulutas@ug.bilkent.edu.tr>
2023-12-22 00:33:09 +0300

    Update WorkshopController.java

--- src/main/java/com/backend/artbase/controllers/WorkshopController.java
+++ src/main/java/com/backend/artbase/controllers/WorkshopController.java
@@ -2,12 +2,14 @@ package com.backend.artbase.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.backend.artbase.dtos.workshop.WorkshopDto;
 import com.backend.artbase.services.WorkshopService;
 import java.util.List;
 
 @RestController
+@RequestMapping("/workshop")
 public class WorkshopController {
 
     private final WorkshopService workshopService;
@@ -16,7 +18,7 @@ public class WorkshopController {
         this.workshopService = workshopService;
     }
 
-    @GetMapping("/workshops")
+    @GetMapping("/all")
     public ResponseEntity<List<WorkshopDto>> getAllWorkshops() {
         List<WorkshopDto> workshops = workshopService.getAllWorkshops();
         return ResponseEntity.ok(workshops);
